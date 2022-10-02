a0d2da8f60ad707ff3ead3dbe477b76dcf058024
David <davidmosquera@uninorte>
2022-10-02 12:04:43 -0500

    test better use of modules

---
+++ Combat.py
@@ -0,0 +1,4 @@
+from Sprites import *
+
+class Combat:
+    pass
--- Preguntas.py
+++ Preguntas.py
@@ -1,4 +1,43 @@
-from Levels import Pregunta
+from typing import List
+from Sprites import Personaje
+class Pregunta:
+    def __init__(self,enunciado:str,opciones: List[str], correcta: int, bono: int) -> None:
+        self.enunciado = enunciado
+        self.opciones = opciones
+        if correcta>=len(self.opciones): # validar creación de Pregunta
+            raise ValueError("Excede el indice maximo")
+        self.correcta = correcta
+        self.intentos = 1
+        self.bono = bono
+        """
+        Constructor de clase de Pregunta
+
+        enunciado: pregunta
+        opciones: opciones de respuesta
+        correcta: indice de respuesta correcta
+        intentos: numero de intentos realizados
+        bono: recompensa en inteligencia 
+        """
+        for i in range(len(self.opciones)): # Agregar numeración a pregunta
+            self.opciones[i]=" <"+str(i+1)+">"+self.opciones[i]
+
+    def hacer(self,player: Personaje):
+        """
+        Realizar pregunta a player
+        """
+        print(self.enunciado)
+        print(self.opciones)
+        res = input()
+        while res!=str(self.correcta+1):
+            print("Incorrecto")
+            self.intentos+=1
+            res = input()
+        player.inteligencia+= (self.bono)-(self.intentos-1)
+        player.atributos()
+        
+    def __repr__(self) -> str:
+        return f"{self.enunciado}"
+
 a = Pregunta("¿Cuál es un metodo ?",
                 [" vida "," cambiar_arma()"," defensa"],1,10) 
 
